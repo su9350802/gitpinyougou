b0caaf0e26261e7d6206ebd69250181d928ea9a0
用户名 <782518961@qq.com>
2019-03-16 15:34:22 +0800

    添加品优购项目

---
+++ parent101/pinyougou-interface/src/main/java/cn/itcast/core/service/brand/BrandService.java
@@ -0,0 +1,10 @@
+package cn.itcast.core.service.brand;
+
+import cn.itcast.core.pojo.good.Brand;
+
+import java.util.List;
+
+public interface BrandService {
+
+   List<Brand> findAll();
+}
---
+++ parent101/pinyougou-web-manager/src/main/java/cn/itcast/core/controller/brand/BrandController.java
@@ -0,0 +1,22 @@
+package cn.itcast.core.controller.brand;
+
+import cn.itcast.core.pojo.good.Brand;
+import cn.itcast.core.service.brand.BrandService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll.do")
+    public List<Brand> findAll() {
+        return brandService.findAll();
+    }
+}
